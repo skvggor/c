d020cb376bfe42d8f8488e806b58cc7584301ede
marcker <marckfree@gmail.com>
2016-04-10 16:40:23 -0300

    Adiciona variável com salário limite (tarefa 4)

--- lista-de-exercicios-1-semestre-algoritmos/4-inss-desconto.c
+++ lista-de-exercicios-1-semestre-algoritmos/4-inss-desconto.c
@@ -3,13 +3,14 @@
 int main(void) {
 	float
 		salario_bruto,
+		salario_liquido = 1200,
 		salario_liquido,
 		desconto = 0.07;
 
 	printf("Qual o seu salario bruto (R$)? ");
 	scanf("%f", & salario_bruto);
 
-	if (salario_bruto <= 1200) {
+	if (salario_bruto <= salario_limite) {
 		salario_liquido = salario_bruto - (salario_bruto * desconto);
 	} else {
 		salario_liquido = salario_bruto;
